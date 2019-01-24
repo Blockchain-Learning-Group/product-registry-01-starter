f73f0d3f7bee7cf03ab5fbb1904db5eaf9085556
Adam J Lemmon <adamjlemmon@gmail.com>
2019-01-24 08:47:28 -0500

    init started pack for reg 1 exercise

---
+++ app.js
@@ -0,0 +1,14 @@
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        Hello, I am your first React component!
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
